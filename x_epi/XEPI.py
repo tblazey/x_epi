6f3ecf020733a103d547cba049f0032261bd87fe
Tyler Blazey <blazey@wustl.edu>
2023-09-08 21:14:44 -0500

    rtd

--- x_epi/XEPI.py
+++ x_epi/XEPI.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-
+import sys
+print(sys.path)
 #Load libraries
 from copy import deepcopy
 import json
--- x_epi/__init__.py
+++ x_epi/__init__.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python
-
+import sys
+print(sys.path)
 from .XEPI import XEPI
+import matplotlib.pyplot as plt
