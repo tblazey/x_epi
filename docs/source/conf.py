04d60eec54e41efdae39646ac14196729f0815ce
Tyler Blazey <blazey@wustl.edu>
2023-09-09 14:45:16 -0500

    Doc updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
-sys.path.insert(0, os.path.abspath('../../bin/'))
-
+sys.path.insert(0, os.path.abspath('../../x_epi/bin'))
+print(os.path.abspath('../../x_epi'))
 # -- Project information
 
 project = 'x_epi'
